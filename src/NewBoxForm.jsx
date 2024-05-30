5db07b491d5d99694c563d171fcd779b722dbc76
Ricardo Perez <rxperez12@gmail.com>
2024-05-30 12:17:24 -0700

    fixed background color bug

--- src/NewBoxForm.jsx
+++ src/NewBoxForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 /** Form for a new box.
  *
- * Props:
+ * Props: TODO: fill out
  *
  * State:
  *
@@ -11,60 +11,65 @@ import { useState } from "react";
  * Structure:
  */
 
-function NewBoxForm({ props_placeholder }){
+function NewBoxForm({ addNewBox }) {
+  const initialState = {
+    height: "",
+    width: "",
+    backgroundColor: "",
+  };
+  const [formData, setFormData] = useState(initialState);
 
-    const initialState = {
-        height:"", width:"", backgroundColor:""
-    }
-    const [formData, setFormData] = useState(initialState)
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    console.log("handle change", evt.target);
 
-    function handleChange(evt){
-        const { name, value } = evt.target
-        console.log("handle change", evt.target)
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value,
+    }));
+  }
 
-        setFormData(formData => ({
-            ...formData,
-            [name]:value
-        }))
-    }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log("submitting a form", evt.target);
+    addNewBox({
+      height: formData.height,
+      width: formData.width,
+      backgroundColor: formData.backgroundColor,
+    });
 
-    function handleSubmit(evt){
-        evt.preventDefault()
-        console.log("submitting a form", evt.target)
-    }
+    setFormData(initialState);
+  }
 
-    return(
-        <div>
-            <h1>Boxy-Topsy</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="height"></label>
-                <input
-                    id="height"
-                    name="height"
-                    value={formData.height}
-                    onChange={handleChange}
-                />
-                <label htmlFor="width"></label>
-                <input
-                    id="width"
-                    name="width"
-                    value={formData.width}
-                    onChange={handleChange}
-                />
-                <label htmlFor="bg-color"></label>
-                <input
-                    id="bg-color"
-                    name="bg-color"
-                    value={formData.backgroundColor}
-                    onChange={handleChange}
-                />
-                <button>
-                    Add a new box!
-                </button>
-            </form>
-        </div>
-    )
-
+  return (
+    <div>
+      <h1>Boxy-Topsy</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="height">Height</label>
+        <input
+          id="NewBoxForm-height"
+          name="height"
+          value={formData.height}
+          onChange={handleChange}
+        />
+        <label htmlFor="width">Width</label>
+        <input
+          id="NewBoxForm-width"
+          name="width"
+          value={formData.width}
+          onChange={handleChange}
+        />
+        <label htmlFor="bg-color">Background Color</label>
+        <input
+          id="NewBoxForm-bg-color"
+          name="backgroundColor"
+          value={formData.backgroundColor}
+          onChange={handleChange}
+        />
+        <button>Add a new box!</button>
+      </form>
+    </div>
+  );
 }
 
-export default NewBoxForm
+export default NewBoxForm;
